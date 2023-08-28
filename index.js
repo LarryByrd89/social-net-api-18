1b925983b4c872f8897260ccfcc36c5797cd61de
LarryByrd89 <laryn.n99@gmail.com>
2023-08-28 00:24:10 -0400

    index and reaction models

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// MongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-net-api-18', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+// API routes
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+const moment = require('moment');
+const { Schema, model } = require('mongoose');
+
+// Define the date format function using Moment
+const dateFormat = timestamp => {
+  return moment(timestamp).format('MMMM Do, YYYY [at] h:mm A');
+};
+
+const reactionSchema = new Schema(
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Reaction = model('Reaction', reactionSchema);
+
+module.exports = Reaction;
